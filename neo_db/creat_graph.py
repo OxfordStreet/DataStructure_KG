065882683a3fa246fe87c3b9e9fc3a8859e2b04c
OxfordStreet <739494361@qq.com>
2021-05-04 14:15:19 +0800

    本地修改与远程fork合并

---
+++ neo_db/creat_graph.py
@@ -0,0 +1,15 @@
+from py2neo import Graph, Node, Relationship
+from config import graph
+
+graph.delete_all()
+graph.run("match (n) detach delete n")
+with open("./raw_data/relation.txt", encoding='utf-8') as f:
+    for line in f.readlines():
+        rela_array = line.strip("\n").split(",")
+        print(rela_array)
+        graph.run("merge (p:Concept {cate:'%s', name: '%s'})" % (rela_array[3], rela_array[0]))
+        graph.run("merge (p: Concept{cate:'%s',name: '%s'})" % (rela_array[4], rela_array[1]))
+        graph.run(
+            "MATCH(e: Concept), (cc: Concept) WHERE e.name='%s' AND cc.name='%s' CREATE(e)-[r:%s{relation: '%s'}]->(cc)\
+            RETURN r" % (rela_array[0], rela_array[1], rela_array[2], rela_array[2])
+        )
